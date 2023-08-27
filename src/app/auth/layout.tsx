c33481a768aa70732fe031a7c1e2f484329ec465
Rexedge <ap.oyeniran@gmail.com>
2023-08-27 20:37:00 +0100

    Auth Layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,19 +1,78 @@
-import AuthComp from '@/components/layout/authComp';
-import Image from "next/image";
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 export default function AuthLayout({
-    children,
-}:{
-    children: React.ReactNode;
+	children,
+}: {
+	children: React.ReactNode;
 }) {
-  return (
-    <div className="flex p-5 h-full">
-      <AuthComp />
-      <div className="lg:ml-20 lg:w-[26%] md:w-[80%] md:m-auto  w-full">
-        <Image src="/authpageLogo.png" width={250} height={250} alt="logo" className='md:w-[70%]'  />
-        {children}
-      </div>
-    </div>
-  );
+	return (
+		<div className='grid grid-cols-1 lg:grid-cols-2 gap-5 p-3 lg:p-5 min-h-screen bg-background'>
+			<div className='hidden lg:flex justify-center items-center w-full h-full p-3 sm:p-5 bg-gradient-to-b from-primary-900 to-primary-700 text-white rounded-2xl'>
+				<div className='flex flex-col justify-between max-w-[550px] h-full'>
+					<div className='flex flex-col gap-12'>
+						<div className='text-h5'>TRANSPAY</div>
+						<div className='flex flex-col gap-4'>
+							<div className='font-bold text-h2 2xl:text-h1'>
+								Ensure Drivers are Checked with Maximum
+								Road Safety!!!
+							</div>
+							<h2 className='text-h5 2xl:text-h4 max-w-[485px]'>
+								Where drivers payment are checked and
+								road safety is ensured and money can be
+								tracked
+							</h2>
+						</div>
+					</div>
+					<div>
+						<div className='w-full flex flex-col gap-7 bg-primary-900 p-7 rounded-2xl'>
+							<div className='text-h5 max-w-[450px]'>
+								{`“Accountability in a civilized society
+								is the stepping stone to development and
+								progressive Environment”.`}
+							</div>
+							<div className='flex items-center gap-8'>
+								<div className='h-[70px] w-[70px] overflow-hidden rounded-full'>
+									<Image
+										src='/avater.png'
+										height={70}
+										width={70}
+										alt='avater'
+										className='h-full w-full object-cover object-center'
+									/>
+								</div>
+								<div className='mt-2'>
+									<div className='text-h5 font-bold'>
+										ISCE Digital Concepts
+									</div>
+									<div className='text-bodyBold'>
+										Governor Anambra state
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className='flex gap-1 justify-center mt-8'>
+							<div className='bg-white w-10 h-2 rounded-sm'></div>
+							<div className='bg-white w-2 h-2 rounded-sm'></div>
+							<div className='bg-white w-2 h-2 rounded-sm'></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='flex h-full rounded-2xl justify-center lg:justify-start'>
+				<div className='flex flex-col w-full gap-10 items-center lg:items-start max-w-[450px] lg:ml-10 xl:ml-20'>
+					<div className='h-48 w-48 flex items-center justify-center'>
+						<Image
+							src='/authpageLogo.png'
+							width={250}
+							height={250}
+							alt='logo'
+							className='dark:invert h-full w-full object-contain'
+						/>
+					</div>
+					<div className='w-full max-w-md'>{children}</div>
+				</div>
+			</div>
+		</div>
+	);
 }
