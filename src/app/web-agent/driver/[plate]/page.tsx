90ae7cdd9b0cf1e0aa1269c7bb482ffe3cf2ed63
Rexedge <ap.oyeniran@gmail.com>
2023-08-29 18:10:08 +0100

    added slug

--- src/app/web-agent/driver/[plate]/page.tsx
+++ src/app/web-agent/driver/[plate]/page.tsx
@@ -1,7 +1,11 @@
-
 import WebAgentViewDriverDetails from '@/components/pages/webagent-driver/viewdriver';
 import React from 'react';
 
-export default function DriverPage() {
-	return <div> <WebAgentViewDriverDetails/> </div>;
+export default function DriverPage({ params }: { params: { plate: string } }) {
+	console.log(params);
+	return (
+		<div>
+			<WebAgentViewDriverDetails plate={params.plate} />
+		</div>
+	);
 }
