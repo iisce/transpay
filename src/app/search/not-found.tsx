5d561762780b5c37196b26e495235904aee3dcf4
fusco <emekaignatius5@gmail.com>
2023-10-31 10:44:23 +0100

    not found file added to search page

---
+++ src/app/search/not-found.tsx
@@ -0,0 +1,56 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const NotFound = () => {
+	return (
+		<section className='max-w-lg mx-auto flex gap-10 flex-col items-center justify-center h-screen'>
+			<div className='h-16'>
+				<Image
+					className='object-contain h-full w-full dark:invert'
+					src={'/logo.png'}
+					alt=''
+					width={400}
+					height={400}
+				/>
+			</div>
+			<div className='flex flex-col gap-5'>
+				<h3 className='uppercase text-center'>
+					Vehicle not found
+				</h3>
+				<span className='text-center'>
+					
+				</span>
+				<div className='flex flex-col gap-3 uppercase'>
+					<Button asChild>
+						<Link
+							className=''
+							href={'/dashboard'}
+						>
+							Go To Dashboard
+						</Link>
+					</Button>
+					<Button asChild>
+						<Link
+							className=''
+							href={'/'}
+						>
+							Create an Account
+						</Link>
+					</Button>
+					<Button asChild>
+						<Link
+							className=''
+							href={'/faqs'}
+						>
+							Faqs
+						</Link>
+					</Button>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default NotFound;
